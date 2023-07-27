88516beeddc5e7fecb08fde226e0990760bfc69e
Ben Elkin <8xe8n-cm@online.de>
2023-07-27 15:26:52 +0200

    WIP

--- src/script1.jl
+++ src/script1.jl
@@ -8,6 +8,6 @@ opts = Dict(:url=>winpath)
 # w = Window(shell(), opts)
 # loadurl(w, winpath)
 w = Window()
-db_url = "https://www.dropbox.com/s/1b7hrp5cbum252z/mainwin.html?dl=0"
-db_url = "https://www.dropbox.com/s/1b7hrp5cbum252z/mainwin.html"
-loadurl(w, db_url)
+# db_url = "https://www.dropbox.com/s/1b7hrp5cbum252z/mainwin.html?dl=0"
+gh_url = "https://raw.githubusercontent.com/Eben60/PackageIABlink.jl/main/html/mainwin.html"
+loadurl(w, gh_url)
