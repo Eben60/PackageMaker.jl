07a666713882537ff5cd5571d5d2d07d070a5517
Ben Elkin <8xe8n-cm@online.de>
2023-07-28 23:48:21 +0200

    getelemval & setelemval

--- src/PackageIABlink.jl
+++ src/PackageIABlink.jl
@@ -2,5 +2,28 @@ module PackageIABlink
 using Blink
 
 winpath = realpath("html/mainwin.html")
+@assert isfile(winpath)
+
+function initcontents(fpath=winpath)
+    contents = open(fpath, "r") do file
+        read(file)
+    end
+    return String(contents)
+end
+
+function mainwin(fpath=winpath)
+    win = Window();
+    wincontent = initcontents(fpath)
+    body!(win, wincontent; async=false)
+    return win
+end
+
+export mainwin
+
+getelemval(win, id) = js(win, Blink.JSString("""document.getElementById("$id").value"""))
+export getelemval
+
+setelemval(win, id, newval) = js(win, Blink.JSString("""el = document.getElementById("$id"); el.value = "$newval";"""))
+export setelemval
 
 end # module PackageIABlink
