23c59e814873531880a1cf1cb8cbf998e4d829f3
Ben Elkin <8xe8n-cm@online.de>
2024-08-17 12:45:33 +0200

    expand all plugins

--- src/defaults.jl
+++ src/defaults.jl
@@ -3,37 +3,46 @@ using DataStructures
 "Default plugins for a package to be registered"
 pgins_registered = DefaultDict(false, Dict{String, Bool}([
     "CompatHelper" => 1,
-    "Git"          => 1,
-    "License"      => 1,
-    "ProjectFile"  => 1,
-    "Readme"       => 1,
-    "SrcDir"       => 1, 
-    "TagBot"       => 1,
-    "Tests"        => 1,    
+    "Dependabot" => 1,
+    "Git" => 1,
+    "GitHubActions" => 1,
+    "License" => 1,
+    "ProjectFile" => 1,
+    "Readme" => 1,
+    "Secret" => 1,
+    "SrcDir" => 1,
+    "TagBot" => 1,
+    "Tests" => 1,   
 ]))
 
-"Default plugins for non-public package"
+"Default plugins for non-registered package"
 pgins_package = DefaultDict(false, Dict{String, Bool}([
     "CompatHelper" => 0,
-    "Git"          => 1,
-    "License"      => 1,
-    "ProjectFile"  => 1,
-    "Readme"       => 1,
-    "SrcDir"       => 1, 
-    "TagBot"       => 0,
-    "Tests"        => 1,    
+    "Dependabot" => 0,
+    "Git" => 1,
+    "GitHubActions" => 0,
+    "License" => 1,
+    "ProjectFile" => 1,
+    "Readme" => 1,
+    "Secret" => 0,
+    "SrcDir" => 1,
+    "TagBot" => 0,
+    "Tests" => 1,   
 ]))
 
 "Default plugins for project"
 pgins_project = DefaultDict(false, Dict{String, Bool}([
     "CompatHelper" => 0,
-    "Git"          => 1,
-    "License"      => 0,
-    "ProjectFile"  => 1,
-    "Readme"       => 0,
-    "SrcDir"       => 0, 
-    "TagBot"       => 0,
-    "Tests"        => 1,    
+    "Dependabot" => 0,
+    "Git" => 1,
+    "GitHubActions" => 0,
+    "License" => 0,
+    "ProjectFile" => 1,
+    "Readme" => 0,
+    "Secret" => 0,
+    "SrcDir" => 0,
+    "TagBot" => 0,
+    "Tests" => 1,    
 ]))
 
 pgins_sets = Dict(["Project" => pgins_project,
--- src/testscripts/script30.jl
+++ src/testscripts/script30.jl
@@ -2,9 +2,9 @@ using PackageInABlink, Blink
 
 isdefined(Main, :win) && close(win)
 
-(;win, initvals, newvals, finalvals, changeeventhandle) = initwin("html/mw30.html")
+(;win, initvals, newvals, finalvals, changeeventhandle) = initwin("html/mw31a.html")
 @show finalvals
 ;
 
-
+# TODO generate GitHubActions / Extra Julia versions to test, as strings as <textarea>
 
