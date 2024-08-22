d9f5fabfb74578a9a766b6f41462b2492cbedb6d
Ben Elkin <8xe8n-cm@online.de>
2024-08-22 12:12:57 +0200

    appears to work

---
+++ src/testscripts/script35.jl
@@ -0,0 +1,10 @@
+using PackageInABlink, Blink
+
+isdefined(Main, :win) && close(win)
+
+(;win, initvals, newvals, finalvals, changeeventhandle) = initwin("html/mw35.html")
+@show finalvals
+;
+
+# TODO generate GitHubActions / Extra Julia versions to test, as strings as <textarea>
+
