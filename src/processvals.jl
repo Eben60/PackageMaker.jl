5dae352a2760383842a5a3c08d593aefab2b42b3
Eben60 <not_a_mail@nowhere.org>
2025-01-05 21:44:41 +0100

    add link to documenter-built documentation

--- src/processvals.jl
+++ src/processvals.jl
@@ -210,22 +210,42 @@ function create_proj(fv)
     return t
 end
 
-function make_docstring(proj_name, docstring)
-    pre_header = "# should you ask why the last line of the docstring looks like that:" *
-        "# it will show the package path when help on the package invoked like help?> $(proj_name)\n" *
-        "# and interpolates to an empty string on CI server, preventing appearing the path in the documentation built there"
+function make_docstring(proj_name, docstring, docslink)
+
+    pre_header = "# should you ask why the last line of the docstring looks like that:\n" *
+        "# it will show the package path when help on the package is invoked like     help?> $(proj_name)\n" *
+        "# but will interpolate to an empty string on CI server, preventing appearing the path in the documentation built there"
     header = "    Package $(proj_name) v\$(pkgversion($(proj_name)))"
     footer = """\$(isnothing(get(ENV, "CI", nothing)) ? ("\\n" * "Package local path: " * pathof($(proj_name))) : "") """
+
+    linkline = isnothing(docslink) ? "" : "\n\nDocs under $(docslink)"
+
     fulldocstring = "$pre_header
+
 \"\"\"
-$header
+$(header)
 
-$docstring
+$(docstring)$(linkline)
 
-$footer
+$(footer)
 \"\"\"
 "
+    return fulldocstring
 end
+
+function extract_docslink(docsfile)
+    file_content = read(docsfile, String)
+    file_content = replace(file_content, "\r\n" => "\n")
+    re_canon = r"\n\s*canonical=\"(.+)\",\s*\n"
+    canonical = match(re_canon , file_content)
+    isnothing(canonical) || return canonical[1]
+
+    re_repo = r"\n\s*repo=\"(.+)\",\s*\n"
+    repo = match(re_repo , file_content)
+    isnothing(repo) && return nothing
+    return "https://$(repo[1])"
+end
+
 
 function add_docstring(gen_options)
     (;proj_name, templ_kwargs, docstring) = gen_options
@@ -235,13 +255,22 @@ function add_docstring(gen_options)
     (;dir, user, host) = templ_kwargs
     proj_main_file = joinpath(dir, proj_name, "src", proj_name * ".jl")
     isfile(proj_main_file) || error("file $proj_main_file not found")
+
+    docsfile = joinpath(dir, proj_name, "docs", "make.jl")
+    if isfile(docsfile)
+        docslink = extract_docslink(docsfile)
+    else
+        docslink = nothing
+    end
+
+    full_docstring = make_docstring(proj_name, docstring, docslink)
+
     file_content = read(proj_main_file, String)
     insertion_range = findfirst("module", file_content)
     isnothing(insertion_range) && error("module not found in file $proj_main_file")
     insertion_point = insertion_range.start
     header = insertion_point == 1 ? "" : file_content[1:(insertion_point-1)] * "\n"
 
-    full_docstring = make_docstring(proj_name, docstring)
     new_content = header * full_docstring * file_content[insertion_point:end]
 
     open(proj_main_file, "w") do f
