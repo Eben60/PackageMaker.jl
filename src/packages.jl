da6da91f09219cbc80a5480b1bc72e00c983b39b
Eben60 <eben60@nowhere.org>
2025-01-09 19:37:25 +0100

    found bug for empty main env

--- src/packages.jl
+++ src/packages.jl
@@ -4,7 +4,7 @@ function default_env_packages()
     julia_version_string = "v$(VERSION.major).$(VERSION.minor)"
 
     default_proj_path = abspath(joinpath("$(homedir())", ".julia/environments/", julia_version_string, "Project.toml"))
-    @assert isfile(default_proj_path)
+    isfile(default_proj_path) || return String[]
 
     packages = collect(keys(TOML.parsefile(default_proj_path)["deps"]))
     sort!(packages)
