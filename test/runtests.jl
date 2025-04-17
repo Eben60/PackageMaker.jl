9dfe9dfb5f4033a156ab3e1469295cf9258a4b88
Eben60 <not_a_mail@nowhere.org>
2025-04-17 23:35:38 +0200

    one more minor edit

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ Aqua.test_all(PackageMaker)
 @safetestset "Dropdown menus" include("src/test_dropdown_menus.jl")
 @safetestset "Process values 1" include("src/test_processvals1.jl")
 @safetestset "Process values 2" include("src/test_processvals2.jl")
-@safetestset "Typedefinitions" include("src/test_typedefs.jl")
+@safetestset "Type definitions" include("src/test_typedefs.jl")
 
 if ! Sys.islinux() # for some reason, errors on CI server on Ubuntu, but is OK on Windows. OK on local Mac.
     @safetestset "Blink window" include("src/test_window.jl")
