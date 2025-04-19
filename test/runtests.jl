a16fb3fd0bc798bbd55830454e05a05ed50e2c3f
Eben60 <not_a_mail@nowhere.org>
2025-04-19 14:09:45 +0200

    debug windows testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using SafeTestsets
 
 @safetestset "Aqua" include("src/test_aqua.jl")
 @safetestset "Dropdown menus" include("src/test_dropdown_menus.jl")
-@safetestset "HTML generation" include("src/test_html.jl")
+Sys.iswindows && (@safetestset "HTML generation" include("src/test_html.jl"))
 @safetestset "Process values 1" include("src/test_processvals1.jl")
 @safetestset "Process values 2" include("src/test_processvals2.jl")
 @safetestset "Type definitions" include("src/test_typedefs.jl")
