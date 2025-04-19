050906d0d52994a347de568f47a16155bbd925e3
Eben60 <not_a_mail@nowhere.org>
2025-04-19 14:22:38 +0200

    debugging WIP2

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using SafeTestsets
 
 @safetestset "Aqua" include("src/test_aqua.jl")
 @safetestset "Dropdown menus" include("src/test_dropdown_menus.jl")
-Sys.iswindows() && (@safetestset "HTML generation" include("src/test_html.jl"))
+if Sys.iswindows() 
+    @safetestset "HTML generation" include("src/test_html.jl")
+end
 @safetestset "Process values 1" include("src/test_processvals1.jl")
 @safetestset "Process values 2" include("src/test_processvals2.jl")
 @safetestset "Type definitions" include("src/test_typedefs.jl")
