73c6d5062e8a5248e8db9d7595027f6cb203c655
Eben60 <not_a_mail@nowhere.org>
2025-04-19 23:27:45 +0200

    add tests

--- test/src/test_window.jl
+++ test/src/test_window.jl
@@ -1,10 +1,13 @@
-# module TestWindow
+# Sys.islinux() && @unsafe # doesn't help
+
 using Blink
 using Test
 using PackageMaker
-using PackageMaker: mainwin, @unsafe
+using PackageMaker: mainwin, initwin, @unsafe, initialize
 
-# Sys.islinux() && @unsafe # doesn't help
+initialize()
+
+
 
 @testset "size Tests" begin
     w = Window(Blink.Dict(:show => false, :width=>150, :height=>100), async=false);
@@ -19,17 +22,24 @@ end
 
 
 @testset "Window with full contents" begin
-    w = mainwin(; test=true);
+    (;win, initvals, newvals, finalvals, changeeventhandle) = initwin(; debug=true);
+    ks = keys(initvals) |> collect |> sort!
+    ks = filter(x->!startswith(string(x), "SavedConfigTag"), ks)
+    ks = filter(x->!startswith(string(x), "Save_Configuration"), ks)  
+    # @show length(ks)
+    @test length(ks) == 93
+    # @show ks
+
+    @test initvals[:GeneralOptions_julia_min_version].value == "1.10.0"
+    @test initvals[:License_name_1].value == "MIT"
     el = shell() # reference to the Electron process
-    @test active(w)
-    close(w)
-    @test ! active(w)
-    close(w)
+    @test active(win)
+    close(win)
+    @test ! active(win)
+    close(win)
     try
         close(el)
     catch
     end
 end
-
-# end
 ;
