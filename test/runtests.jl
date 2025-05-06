abd04b1a2dcdd2460bd2ae9ed7b871cf329253d9
Eben60 <not_a_mail@nowhere.org>
2025-05-06 18:58:53 +0200

    test saving, reading, deleting settings from simulated GUI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 # using PackageMaker
 using SafeTestsets
 
+TEST_SETTING::String = "z2test_TestSetting_1681796"
+
+
 @safetestset "Aqua" include("src/test_aqua.jl")
 @safetestset "Dropdown menus" include("src/test_dropdown_menus.jl")
 # Sys.isunix() &&
@@ -13,5 +16,5 @@ using SafeTestsets
 @safetestset "Create project 2" include("src/test_create_pkg02.jl")
 
 if  ! Sys.islinux() # for some reason, errors on Ubuntu both on CI server and on local computer, but is OK on Windows. OK on local Mac.
-    @safetestset "Blink window" include("src/test_window.jl") # broken
+    @safetestset "Blink window" include("src/test_window.jl")
 end
